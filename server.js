2ab69baa3e0104fde90da92dd2a9f0aa9ca016b1
dhimashary <dhimas.hary@gmail.com>
2022-08-16 21:56:52 +0700

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const server = require('./socketConfig')
-const http = require('./bin/http')
-server.attach(http)
+const server = require('./socketConfig');
+const http = require('./bin/http');
+server.attach(http);
 
 // nodemon server.js
--- socketConfig.js
+++ socketConfig.js
@@ -1,13 +1,18 @@
-const io = require('socket.io')();
-
-io.on('connection', socket => {
-
+// const io = require('socket.io')();
+const httpServer = require('./bin/http');
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+io.on('connection', (socket) => {
   // all socket listener here
-  socket.on("send-message", (data) => {
-    io.emit("room-detail", data);
+  socket.on('send-message', (data) => {
+    console.log(data);
+    io.emit('room-detail', data);
   });
-  socket.on("typing-start", (data) => {
-    io.emit("typing-start", data);
+  socket.on('typing-start', (data) => {
+    io.emit('typing-start', data);
   });
 });
 
