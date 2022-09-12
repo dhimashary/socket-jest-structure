fca4e484daf24024f479734194a6d52adc69b504
Hary Dhimas <dhimas.hary@gmail.com>
2022-09-12 11:13:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.get((req, res) => res.status(200).json({msg: 'ok'}))
+
 // all your express middleware here
 
 module.exports = app
